4424588432ec60eec7d6e3a4cfe4183b19cd3978
AlexandreCoding <alexandre.alonso@softwareone.com>
2021-10-07 12:13:23 +0200

    Added comments to clarify functionality of every method

--- Game.cs
+++ Game.cs
@@ -4,6 +4,12 @@ namespace ConsoleGame
 {
   class Game : SuperGame
   {
+
+// Update position of player cursor by coordinates
+// 
+// Parameters: last key pressed by player, out X coordinate and out Y coordinate of player position.
+// Doesn't return anything, void.
+
     public new static void UpdatePosition (string keyPressed, out int xCoord, out int yCoord) {
       yCoord = 0;
       xCoord= 0;
@@ -26,6 +32,12 @@ namespace ConsoleGame
       break;
       }
   }
+
+// Update cursor character by the orientation of last key input
+// 
+// Parameters: last key pressed by player.
+// Returns a new char value if player has changed the direction of movement.
+
   public new static char UpdateCursor (string keyPressed) {
     switch (keyPressed) {
       case "LeftArrow":
@@ -41,6 +53,11 @@ namespace ConsoleGame
     }
   }
 
+// Console limits applied so player cursor cannot go to negative coords or higher coords than a maxValue
+// 
+// Parameters: Coord to compare and the highest value player will be able to reach.
+// Returns a new int value if player is trying to get to unreachable coordinaaes.
+
   public new static int KeepInBounds(int coord, int maxValue) {
     int newCoord;
       if (coord < 0) {
@@ -54,6 +71,11 @@ namespace ConsoleGame
         return newCoord;
   }
   }
+
+// Checks if player reached the fruit/goal position.
+//
+// Parameters: X coords and Y coords for player cursor position and fruit/goal position.
+// Returns true if player coords and fruit coords are equal otherwise returns false.
 
   public new static bool DidScore (int xPlayer, int yPlayer, int xFruit, int yFruit) {
     if ((xPlayer == xFruit) && (yPlayer == yFruit)) {
